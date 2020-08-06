81738a3855b3c9d53e9410045597b0dcc7ab859c
Jason Easterly <jason.easterly11@gmail.com>
2020-08-06 19:35:03 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -18,5 +18,4 @@
 
 puts "You know what year it is??"
 this_year = 2020
-puts "No, I don't" unless this_year == 2019 
-puts "Hey it's 2019!"
+puts "No, I don't" unless this_year == 2019 puts "Hey it's 2019!"
