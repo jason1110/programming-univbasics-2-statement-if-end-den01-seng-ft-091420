50aa81ddb5c3c2b7d1b438f1e88d72615858cd92
Jason Easterly <jason.easterly11@gmail.com>
2020-08-06 19:31:01 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -16,11 +16,6 @@
 #  puts
 #end
 
-chance_of_rain = -23
-if chance_of_rain <= 0.25
-  puts "Pack a sun shelter!"
-elsif (chance_of_rain > 0.25 && chance_of_rain < 0.75)
-  puts "Pack an umbrella!"
-else
-  puts "Stay home and read Hegel."
-end
+puts "You know what yesr it is??"
+this_year = 2019
+puts "Hey, it's 2019!" if this_year == 2019
