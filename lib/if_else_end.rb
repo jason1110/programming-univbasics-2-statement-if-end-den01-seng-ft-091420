44ad214f19bfad6f77a5b110ddb7c608180989c7
Jason Easterly <jason.easterly11@gmail.com>
2020-08-06 19:24:17 +0000

    Automatically backed up by Learn

--- lib/if_else_end.rb
+++ lib/if_else_end.rb
@@ -1,18 +1,24 @@
-if <something true or false>
-  puts "This code runs!"
-end
+#if <something true or false>
+#  puts "This code runs!"
+#end
 
-if
-  puts
-else
-  puts
-end
+#if
+#  puts
+#else
+#  puts
+#end
 
-if
-  puts
-elsif
-  puts
-else
-  puts
-end
+#if
+#  puts
+#elsif
+#  puts
+#else
+#  puts
+#end
 
+run_code_inside = true
+puts "Code before if...end"
+if run_code_inside
+  puts "code inside"
+end
+puts "Code after if...end"
